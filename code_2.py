3b1447a8dc16d82d33c29b08c40f79bd0bdfe84b
Polina <p9991820@gmail.com>
2023-11-16 12:22:22 +0200

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,17 +1,26 @@
-    const data = [];
-main();
-}
-function main() {
-    const data = generateRandomData();
-    data.forEach(item => {
-
-
-
-    for (let i = 0; i < 10; i++) {
-function generateRandomData() {
-        console.log("Random Number: " + item);
-}
-    });
-        data.push(Math.floor(Math.random() * 100) + 1);
-    return data;
+        for (int item : data) {
+        for (int i = 0; i < 10; i++) {
+        int[] data = new int[10];
+            data[i] = (int) (Math.random() * 100) + 1;
     }
+        }
+        }
+
+
+}
+            System.out.println("Random Number: " + item);
+    public static void main(String[] args) {
+public class RandomDataGenerator {    for item in data:
+def generate_random_data():
+import random
+
+if __name__ == "__main__":
+
+    main()
+
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+def main():
+    return data
+        print(f"Random Number: {item}")
