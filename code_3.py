3d3a74e88023fdb9efc2bb2acd054981de3df057
Polina <p9991820@gmail.com>
2023-11-22 02:48:45 +0200

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,13 +1,13 @@
+if __name__ == "__main__":
+def main():
+    main()
 
-        for (int i = 0; i < 10; i++) {
-            data[i] = (int) (Math.random() * 100) + 1;
-        }
-    public static void main(String[] args) {
-        int[] data = new int[10];
-        for (int item : data) {
-public class RandomDataGenerator {
-        }
-}
-            System.out.println("Random Number: " + item);
 
-    }
+    data = generate_random_data()
+import random
+        print(f"Random Number: {item}")
+    for item in data:
+    return data
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+
